232f1ce4cb374d81242eeea0305871376a4cb37e
t170815518 <christang_1023@outlook.com>
2020-09-13 16:29:00 +0800

    Add makefiles

---
+++ cmake-build-debug/CMakeFiles/lab.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/tangyuting/Study/CZ2001/lab/BruteForce.c" "/Users/tangyuting/Study/CZ2001/lab/cmake-build-debug/CMakeFiles/lab.dir/BruteForce.c.o"
+  "/Users/tangyuting/Study/CZ2001/lab/FNAFilePreprocess.c" "/Users/tangyuting/Study/CZ2001/lab/cmake-build-debug/CMakeFiles/lab.dir/FNAFilePreprocess.c.o"
+  "/Users/tangyuting/Study/CZ2001/lab/KMPAlgorithm.c" "/Users/tangyuting/Study/CZ2001/lab/cmake-build-debug/CMakeFiles/lab.dir/KMPAlgorithm.c.o"
+  "/Users/tangyuting/Study/CZ2001/lab/RabinKarpAlgorithm.c" "/Users/tangyuting/Study/CZ2001/lab/cmake-build-debug/CMakeFiles/lab.dir/RabinKarpAlgorithm.c.o"
+  "/Users/tangyuting/Study/CZ2001/lab/main.c" "/Users/tangyuting/Study/CZ2001/lab/cmake-build-debug/CMakeFiles/lab.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab.dir/BruteForce.c.o"
+  "CMakeFiles/lab.dir/FNAFilePreprocess.c.o"
+  "CMakeFiles/lab.dir/KMPAlgorithm.c.o"
+  "CMakeFiles/lab.dir/RabinKarpAlgorithm.c.o"
+  "CMakeFiles/lab.dir/main.c.o"
+  "lab"
+  "lab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
